868556f8287e1070f9a98f30e4c6b2473cf86ffe
Scott Davies <scottdavies1983@gmail.com>
2021-04-07 06:18:32 +0100

    finished next.js intro module

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
 import Page from "../components/Page"; // Option space command for quick import
-
+// Minute 13:03 Creating a page layout
 /* eslint-disable react/prop-types */
 export default function MyApp({ Component, pageProps }) {
   return (
