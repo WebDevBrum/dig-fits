0064b6c620b5ba0c12ca729c4aef04851fb698ef
Scott Davies <scottdavies1983@gmail.com>
2021-04-22 20:43:24 +0100

    visualising route changes with NProgress and Next router events

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
-
+import NProgress from "nprogress";
+import Router from "next/router";
 import Page from "../components/Page"; // Option space command for quick import
 // Minute 13:03 Creating a page layout
 /* eslint-disable react/prop-types */
+
+// TODO Swap with our own
+import "../components/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <Page>
