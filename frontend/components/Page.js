dfebef1d0a7c16eaba6aa8f0f1c4b9a5cb39f843
Scott Davies <scottdavies1983@gmail.com>
2021-04-22 12:55:04 +0100

    Global Styles, Typography and Layout Styles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Header from "./Header";
 
 const GlobalStyles = createGlobalStyle`
@@ -28,8 +28,28 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: inherit;
   }
   body  {
-    font-family: 'radnika_next', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'radnika_next', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
   }
+  a {
+    text-decoration: none;
+    color: var(---black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  button {
+    font-family: 'radnika_next', ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxwidth);
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
 export default function Page({ children, cool }) {
@@ -37,9 +57,7 @@ export default function Page({ children, cool }) {
     <div>
       <GlobalStyles />
       <Header />
-      <h2>I am the page component</h2>
-      <h3>{cool}</h3>
-      {children}
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 }
