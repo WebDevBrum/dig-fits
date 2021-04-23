c5750920687fd8060e405efaabc2d3eaf5dd08ab
Scott Davies <scottdavies1983@gmail.com>
2021-04-23 05:37:16 +0100

    fixing styled components flicker on server render

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-undef */
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en">
