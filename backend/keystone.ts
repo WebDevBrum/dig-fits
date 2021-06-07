cafd81c5a2a8fefc7bef3cc2a626ab1cfc70c3f1
Scott Davies <scottdavies1983@gmail.com>
2021-06-07 05:56:28 +0100

    uploading product images, creating two way data relationships in keystone and inserting seed data

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,9 +1,11 @@
 import { createAuth } from '@keystone-next/auth'
 import { User } from './schemas/User';
-import { Product } from './schemas/Product'
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
 import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -32,12 +34,18 @@ export default withAuth(config({
   db: {
     adapter: 'mongoose',
     url: databaseURL,
-    //TODO: Add data seeding here
+    async onConnect(keystone) {
+      console.log('Connected to the database');
+      if (process.argv.includes('--seed-data')) {
+        await insertSeedData(keystone);
+      }
+    },
   },
   lists: createSchema({
     // Schema items go in here
     User,  //same as User: User
-    Product
+    Product,
+    ProductImage,
   }),
   ui: {
     // show the UI only for people who pass this test
