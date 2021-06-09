e4728a9a693a78beee95348631bfc4b5d1782ef9
Scott Davies <scottdavies1983@gmail.com>
2021-06-09 06:01:16 +0100

    Fetching Data with hooks and Displaying it in our Front End

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,7 +1,42 @@
+import { useQuery } from "@apollo/client";
+import styled from "styled-components";
+import gql from "graphql-tag";
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 export default function Products() {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  console.log(data, error, loading);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>error.message</p>;
   return (
-    <div>
-      <p>Products</p>
-    </div>
+    <>
+      <ProductsListStyles>
+        {data.allProducts.map((product) => (
+          <p key={product.id}>{product.name}</p>
+        ))}
+      </ProductsListStyles>
+    </>
   );
 }
