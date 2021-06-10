70d48deb067f1f364891d994b24da460bb036b8c
Scott Davies <scottdavies1983@gmail.com>
2021-06-10 06:10:45 +0100

    set up createProduct form with custom React hooks

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,43 @@
+import useForm from "../lib/useForm";
+
+export default function CreateProduct() {
+  // Custom hook
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    name: "cool shoes",
+    price: 12345,
+    description: "i like these shoes",
+  });
+
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="Price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="button" onClick={clearForm}>
+        Clear Form
+      </button>
+      <button type="button" onClick={resetForm}>
+        Reset Form
+      </button>
+    </form>
+  );
+}
